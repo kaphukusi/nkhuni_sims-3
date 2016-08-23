eee5af65070469a9a4463623fa9eaf6830247cf6
Hope Kaphukusi <hopekaphukusi@gmail.com>
2016-08-23 21:05:53 +0200

    added bootstrap

--- app.js
+++ app.js
@@ -5,14 +5,17 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
+app.engine('.html', require('ejs').__express);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -25,11 +28,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
+/* views */
+app.use('services', routes);
+app.use('contact', routes);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
+});
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 // error handlers
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,28 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Nkhoma University' });
+});
+
+router.get('/courses', function(req, res, next) {
+  res.render('courses', { title: 'Our Courses' });
+});
+
+router.get('/events', function(req, res, next) {
+  res.render('events', { title: 'Nkhoma University Events' });
+});
+
+router.get('/gallery', function(req, res, next) {
+  res.render('gallery', { title: 'Nkhoma University Gallery' });
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Our Services' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Us' });
+});
+
 module.exports = router;
