cf33b6f6a7fb0492220a1958e42e1bb7c1b55ced
Hope Kaphukusi <hopekaphukusi@gmail.com>
2016-08-24 18:28:36 +0200

    several views

--- routes/index.js
+++ routes/index.js
@@ -14,20 +14,20 @@ router.get('/view_students', function(req, res, next) {
   res.render('view_students', { title: 'SIMS | View Students' });
 });
 
-router.get('/events', function(req, res, next) {
-  res.render('events', { title: 'Nkhoma University Events' });
+router.get('/new_falculty', function(req, res, next) {
+  res.render('new_falculty', { title: 'SIMS | Add Falculty' });
 });
 
-router.get('/gallery', function(req, res, next) {
-  res.render('gallery', { title: 'Nkhoma University Gallery' });
+router.get('/view_falculties', function(req, res, next) {
+  res.render('view_falculties', { title: 'SIMS | View Falculties' });
 });
 
-router.get('/services', function(req, res, next) {
-  res.render('services', { title: 'Our Services' });
+router.get('/new_department', function(req, res, next) {
+  res.render('new_department', { title: 'SIMS | New Department' });
 });
 
-router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact Us' });
+router.get('/view_departments', function(req, res, next) {
+  res.render('view_departments', { title: 'SIMS | View Departments' });
 });
 
 router.get('/student', function(req, res, next) {
